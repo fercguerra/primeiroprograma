cfe46d88f410445434230d5c9de7a9df57720ba6
Fernanda Cristina Guerra <60983422+fercguerra@users.noreply.github.com>
2021-12-07 22:17:36 -0300

    Initial commit

---
+++ src/primeiroprograma/nome.java
@@ -0,0 +1,22 @@
+package primeiroprograma;
+
+import java.util.Scanner;
+
+public class nome {
+
+	public static void main(String[] args) {
+		System.out.println("Digite seu nome: ");
+		
+		String  nome;
+		
+		Scanner sc = new Scanner (System.in);
+		
+		nome = sc.next();
+		
+		System.out.println(nome);
+		
+				
+
+	}
+
+}
